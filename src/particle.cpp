d766e073332407a4d18234ca7615f518cc21058b
NCJ <me@ncj.wiki>
2021-06-14 00:35:09 +0800

    CUDA!!!!!!

--- src/particle.cpp
+++ src/particle.cpp
@@ -5,6 +5,6 @@
 #include "particle.h"
 
 
-Particle::Particle() : position(random<Real>(0.2, 0.7), random<Real>(0.3, 0.9)), velocity(0,-1),
+Particle::Particle() : position(random<Real>(0.2, 0.7), random<Real>(0.3, 0.9)), velocity(0,-10),
                        J(1), C() {
 }

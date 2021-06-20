aabeed488567196fc436f5ab468076cd4bee76c6
ybc3100356 <441903690@qq.com>
2021-06-20 16:41:04 +0800

    Set camera; Tune parameters

--- src/particle.cpp
+++ src/particle.cpp
@@ -5,6 +5,10 @@
 #include "particle.h"
 
 
-Particle::Particle() : position(random<Real>(0.2, 0.7), random<Real>(0.3, 0.9), random<Real>(0.1, 0.9)), velocity(-10, -10, 0),
-                       J(1), C(mat3()) {
+Particle::Particle() : position(
+        random<Real>(0.05, 0.55),
+        random<Real>(0.05, 0.55),
+        random<Real>(0.05, 0.55)),
+        velocity(0, -1, 0),
+                       J(1), C(mat3(0)) {
 }
